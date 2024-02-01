d2aa2ced6206ae08943ab25a654c824fb657486c
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2024-02-01 13:09:21 +0530

    Update form.js

--- form.js
+++ form.js
@@ -74,7 +74,7 @@ form.addEventListener("submit", async function(event) {
         method: "POST",
         headers: {
             "Authorization": "Bearer 1000.a10b8f629862b326d63b35211a58afb7.089fb5303ec39042079dead131d12d34",
-            "Content-Type": "application/x-www-form-urlencoded, multipart/form-data",
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(jsonData),
     }).then(function(response) {
